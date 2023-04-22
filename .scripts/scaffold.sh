e9e771b2b169b50842431d4e9b0a79de0d56407d
divopsor <divopsor@gmail.com>
2023-04-22 17:41:04 +0900

    init lerna config

--- .scripts/scaffold.sh
+++ .scripts/scaffold.sh
@@ -7,16 +7,9 @@ read -p "Enter package name 👉 " name
 
 if [[ -z $name ]]; then
   exit 1
-else
-  if [[ ! -z $(git add -A; git status --short | grep '^[MARCD]';) ]]
-  then
-    git stash
-    tput setaf 1
-    echo "stashed local changes"
-    tput sgr0
-  fi
-
-  npx hygen package new --name $name
 fi
+
+
+npx hygen package new --name $name
 
 yarn
