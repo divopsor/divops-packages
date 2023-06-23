a47ef7988682acd807cec637aea822a22ebf46d9
divopsor <divopsor@gmail.com>
2023-06-23 18:49:55 +0900

    feat(init-berry): yarn dlx init-berry 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
 module.exports = {
   root: true,
+
   env: { es6: true, node: true },
+
   parser: '@typescript-eslint/parser',
+
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ['import', '@typescript-eslint'],
+  settings: {
+    'import/ignore': ['node_modules'],
+  },
   rules: {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
@@ -21,5 +25,47 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/require-await': 'off',
+
+    "@typescript-eslint/strict-boolean-expressions": 3,
+
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'util',
+            importNames: ['isArray'],
+            message: '`Array.isArray`를 대신 사용해주세요!',
+          },
+        ],
+      },
+    ],
+
+    // https://typescript-eslint.io/linting/troubleshooting/performance-troubleshooting#eslint-plugin-import
+    'import/named': 'off',
+    'import/namespace': 'off',
+    'import/default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-cycle': 'off',
+    'import/no-unused-modules': 'off',
+    'import/no-deprecated': 'off',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external', 'internal'], 'parent', 'sibling', 'index'],
+        alphabetize: { order: 'asc' },
+      },
+    ],
+
+    'semi': 'error',
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1
+      }
+    ],
   },
 };
---
+++ packages/tools/init-berry/src/index.ts
@@ -0,0 +1,13 @@
+import { Cli } from 'clipanion';
+import { MainCommand } from './commands/MainCommand';
+
+const [node, app, ...args] = process.argv;
+
+const cli = new Cli({
+  binaryLabel: `My Application`,
+  binaryName: `${node} ${app}`,
+  binaryVersion: `1.0.0`
+});
+
+cli.register(MainCommand);
+cli.runExit(args);
