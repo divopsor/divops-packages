00764e8a4857e80a7e801777f1e9597717c54991
divopsor <divopsor@gmail.com>
2023-08-08 22:20:02 +0900

    fix(ensureEnv): specify return value

--- packages/node-utils/src/index.ts
+++ packages/node-utils/src/index.ts
@@ -1,7 +1,1 @@
-export function ensureEnv(key: string) {
-  const value = process.env['key'] ?? '';
-
-  if (value === '') {
-    throw new Error(`process.env.${key} is not exists.`);
-  }
-}
+export * from './ensureEnv';
