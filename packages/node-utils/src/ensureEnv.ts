fee44cac9959bb64f391a12314e274cfff27fe60
dp <126376945+divopsor@users.noreply.github.com>
2023-08-08 22:23:49 +0900

    fix(ensureEnv): specify return value (#36)

---
+++ packages/node-utils/src/ensureEnv.ts
@@ -0,0 +1,9 @@
+export function ensureEnv(key: string) {
+  const value = process.env['key'] ?? '';
+
+  if (value === '') {
+    throw new Error(`process.env.${key} is not exists.`);
+  }
+
+  return value;
+}
