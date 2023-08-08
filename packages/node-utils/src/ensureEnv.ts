740c50cf238253a3309fb553c3366145577286ec
divopsor <divopsor@gmail.com>
2023-08-08 23:07:56 +0900

    fix(ensureEnv): key 로 정확하게 수정

--- packages/node-utils/src/ensureEnv.ts
+++ packages/node-utils/src/ensureEnv.ts
@@ -1,5 +1,5 @@
 export function ensureEnv(key: string) {
-  const value = process.env['key'] ?? '';
+  const value = process.env[key] ?? '';
 
   if (value === '') {
     throw new Error(`process.env.${key} is not exists.`);
