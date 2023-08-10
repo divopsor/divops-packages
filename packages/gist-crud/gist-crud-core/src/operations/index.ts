545e7a09eac4e852474064f0674c8261489d9126
divopsor <divopsor@gmail.com>
2023-08-10 13:29:39 +0900

    fix: updateItems 추가

--- packages/gist-crud/gist-crud-core/src/operations/index.ts
+++ packages/gist-crud/gist-crud-core/src/operations/index.ts
@@ -3,6 +3,7 @@ export * from './createItem';
 export * from './readList';
 export * from './readItem';
 export * from './updateItem';
+export * from './updateItems';
 export * from './deleteItem';
 export * from './deleteList';
 export * from './existsList';
---
+++ packages/gist-crud/gist-crud-core/src/operations/updateItems.ts
@@ -0,0 +1,26 @@
+import { Context } from '../context';
+import { List, Item } from '../models/index';
+import { parseRawItems } from '../utils/parseRawItems';
+import { serializeItems } from '../utils/serializeItems';
+import { RawItemRecord } from '../utils/types';
+
+export interface UpdateItemsOptions {
+  listId: List['id'];
+  items: Item[];
+}
+
+export async function updateItems({ listId, items }: UpdateItemsOptions, context: Context): Promise<Item[]> {
+  const { octokit } = context;
+
+  const { data } = await octokit.rest.gists.update({
+    gist_id: listId,
+    files: serializeItems(items),
+    public: false,
+  });
+
+  if (data.files == null) {
+    throw new Error(`존재하지 않습니다.`);
+  }
+
+  return parseRawItems(data.files as RawItemRecord);
+}
