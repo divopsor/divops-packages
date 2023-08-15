c139a5130d92f6ca620252d6f40d84ff6872ae12
dp <126376945+divopsor@users.noreply.github.com>
2023-08-15 18:28:05 +0900

    fix(header): height and click error (#41)

--- packages/ui/src/Header/Header.tsx
+++ packages/ui/src/Header/Header.tsx
@@ -1,36 +1,37 @@
 import { HTMLAttributes, forwardRef } from 'react';
 import { HeaderLogo } from './HeaderLogo';
 
-export type HeaderProps = HTMLAttributes<HTMLDivElement>;
+export interface HeaderProps extends HTMLAttributes<HTMLDivElement> {
+  height?: number;
+}
 
 export const HeaderImpl = forwardRef<HTMLElement, HeaderProps>((props, ref) => {
-  const { children, ...restProps } = props;
+  const { height = 48, children, ...restProps } = props;
 
   return (
     <>
+      <div
+        style={{
+          position: 'relative',
+          top: 0,
+          height: `${height}px`,
+          width: '100%',
+        }}
+      />
+
       <header
         ref={ref}
         style={{
           backgroundColor: 'white',
           position: 'fixed',
           top: 0,
-          height: '48px',
+          height: `${height}px`,
           width: '100%',
         }}
         {...restProps}
       >
         {children}
       </header>
-
-      <div
-        style={{
-          position: 'relative',
-          top: 0,
-          height: '48px',
-          width: '100%',
-        }}
-
-      />
     </>
   );
 });
