ac730da2b49e88af825fd79b5eb9c63b573ef2d0
dp <126376945+divopsor@users.noreply.github.com>
2023-12-25 18:07:06 +0900

    feat(ui): card 추가 (#43)

---
+++ packages/ui/src/Card/Card.tsx
@@ -0,0 +1,41 @@
+import { HTMLAttributes, forwardRef, useState } from 'react';
+
+export type CardProps = HTMLAttributes<HTMLDivElement> & {
+  useHover?: boolean;
+};
+
+export const CardImpl = forwardRef<HTMLDivElement, CardProps>((props, ref) => {
+  const { useHover = false } = props;
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHover(false);
+  };
+
+  const divStyle: CardProps['style'] = {
+    border: '1px solid black',
+    borderRadius: '16px',
+    padding: '8px 16px',
+    margin: '4px',
+    ...(useHover ? ({ backgroundColor: isHover ? '#c8c8c8' : '#F0F0F0' }) : {}),
+    ...(useHover ? ({ cursor: 'pointer' }) : {}),
+  };
+
+  return (
+    <div
+      ref={ref}
+      style={divStyle}
+      onMouseEnter={useHover ? handleMouseEnter : () => {}}
+      onMouseLeave={useHover ? handleMouseLeave : () => {}}
+      {...props}
+    />
+  );
+});
+
+CardImpl.displayName = 'Card';
+
+export const Card = Object.assign(CardImpl);
