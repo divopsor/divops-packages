084f2bc3d2a43c8977a88d34a796dd41e50541e7
divopsor <divopsor@gmail.com>
2023-12-28 19:56:55 +0900

    update only rendered

--- packages/blog-creco-dev/src/components/GlobalCss/GlobalCss.tsx
+++ packages/blog-creco-dev/src/components/GlobalCss/GlobalCss.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import { GLOBAL_CSS } from './constants';
 
+function useRendered() {
+  const [isRendered, setIsRendered] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
+
+  return isRendered;
+}
+
 export const GlobalCss = ({ additionalCss }: { additionalCss?: string }) => {
+  const isRendered = useRendered();
+
   return (
     <style>
       {GLOBAL_CSS}
-      {additionalCss == null ? '' : additionalCss}
+      {isRendered ? additionalCss : null}
     </style>
   );
 };
