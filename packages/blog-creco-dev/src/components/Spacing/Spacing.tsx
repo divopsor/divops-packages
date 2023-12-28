be5c745e77b52edbeea56c435505e624ec85a84a
divopsor <divopsor@gmail.com>
2023-12-28 18:49:16 +0900

    feat: add spacing

---
+++ packages/blog-creco-dev/src/components/Spacing/Spacing.tsx
@@ -0,0 +1,11 @@
+export function Spacing({ size }: { size: number }) {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: `${size}px`,
+      }}
+    >
+    </div>
+  );
+}
--- packages/blog-creco-dev/src/components/index.ts
+++ packages/blog-creco-dev/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './GlobalCss';
 export * from './Post';
 export * from './ResponsivePage';
+export * from './Spacing';
