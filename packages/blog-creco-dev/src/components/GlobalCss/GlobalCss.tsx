a9b204340b43c811d48d20f8ffc3c7cdb6f7dcb8
divopsor <divopsor@gmail.com>
2023-12-28 19:46:22 +0900

    fix: globalCss

--- packages/blog-creco-dev/src/components/GlobalCss/GlobalCss.tsx
+++ packages/blog-creco-dev/src/components/GlobalCss/GlobalCss.tsx
@@ -2,10 +2,7 @@ import { GLOBAL_CSS } from './constants';
 
 export const GlobalCss = ({ additionalCss }: { additionalCss?: string }) => {
   return (
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // eslint-disable-next-line react/no-unknown-property
-    <style global jsx>
+    <style>
       {GLOBAL_CSS}
       {additionalCss == null ? '' : additionalCss}
     </style>
